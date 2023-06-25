2ec79bb74cb643df82ec56c2a71bd3fff505c885
PrashantiWaware <137686962+PrashantiWaware@users.noreply.github.com>
2023-06-25 15:17:55 +0530

    Add files via upload

---
+++ TestConfig.java
@@ -0,0 +1,14 @@
+package ExcelSheet;
+
+import DemoonExcelSheet.Config;
+
+public class TestConfig extends Config {
+
+	public static void main(String[] args) {
+		TestConfig obj=new TestConfig();
+		//obj.readExcelsheet();
+		obj.readAllData();
+
+	}
+
+}
