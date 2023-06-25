c488d6f1e79ccc865417f79a437bb5a9448bb103
PrashantiWaware <137686962+PrashantiWaware@users.noreply.github.com>
2023-06-25 15:22:55 +0530

    Add files via upload

---
+++ SignUp.java
@@ -0,0 +1,20 @@
+package DemoTESTNG2;
+
+import org.testng.annotations.Test;
+
+public class SignUp {
+	
+	
+	@Test
+	public void signUp1() {
+		System.out.println("===signup1===");
+	}
+	
+	
+	
+	@Test
+	public void signUp2() {
+		System.out.println("===signup2===");
+
+}
+}
