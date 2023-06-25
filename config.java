76725f1d8a6c37210114914b68ba7bedc17b8beb
PrashantiWaware <137686962+PrashantiWaware@users.noreply.github.com>
2023-06-25 15:16:08 +0530

    Add files via upload

--- config.java
+++ config.java
@@ -1,52 +1,50 @@
-package Modify_code;
+package FileAccess;
 
-import org.openqa.selenium.By;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class config {
+public class config  {
 	WebDriver driver;
 	
-	public void driverSetting(String url) {
+	public void driverSetting() {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe.exe");
-		 driver=new ChromeDriver();
-		 
-		 driver.get(url);
+		driver=new ChromeDriver();
+		driver.get(accessProperties());
 		
 	}
 	
-	public void iframeAccess() {
-		//driver.switchTo().frame(1);
-		driver.switchTo().frame("webform");
-		//WebElement iframeElement=driver.findElement(By.xpath("//*[@id=\"grwf2_S4ibL_oiktt\"]/iframe"));
-		//driver.switchTo().frame(iframeElement);
+	
+	
+	public String accessProperties() {
+		String strUrl=" ";
+		
+		
 		
 		try {
-			Thread.sleep(5000);
-			//driver.findElement(By.xpath("//*[@id=\"webform_preview\"]/div")).click();
-		}
-		catch(InterruptedException e) {
-			e.printStackTrace();
-		}
-		catch(Exception e){
-			e.printStackTrace();
+			FileInputStream fis=
+					 new FileInputStream("C:\\SeleniumWorkSpace\\Configuration files\\config.properties");
 			
+			Properties props=new Properties();
+			props.load(fis);
+			System.out.println("Usernamr=> "+props.getProperty("username"));
+			System.out.println("Password=> "+props.getProperty("password"));
+			System.out.println("URL=> "+props.getProperty("url"));
+			
+			strUrl=props.getProperty("url");
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
+		return  strUrl;
 		
-		}
-	
-	public WebElement elementAccess(String elementxpath) {
-		return driver.findElement(By.xpath(elementxpath));
 	}
-	
-	public void loginAccess() {
-		WebElement loginElement=elementAccess("/html/body/div/div/form/div/div[4]/div/div[2]/div[1]/div/input");
-		loginElement.sendKeys("waware@gmail.com");
-		
-		WebElement buttonElement=elementAccess("/html/body/div/div/form/div/div[5]/div/button/span");
-		 buttonElement.click();
-	}
-	
 
 }
