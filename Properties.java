66ff57cf19113a981486f88269bc3ba6ad126236
PrashantiWaware <137686962+PrashantiWaware@users.noreply.github.com>
2023-06-25 15:16:35 +0530

    Add files via upload

---
+++ Properties.java
@@ -0,0 +1,15 @@
+package TestFileAccess;
+
+import FileAccess.config;
+
+public class Properties extends config {
+
+	public static void main(String[] args) {
+		Properties obj=new Properties();
+		//obj.accessProperties();
+		obj.driverSetting();
+		
+
+	}
+
+}
