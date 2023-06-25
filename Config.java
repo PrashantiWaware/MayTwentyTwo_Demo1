b1c30a3e32f7f5a364b7835c5d8a4cea3b5cc40e
PrashantiWaware <137686962+PrashantiWaware@users.noreply.github.com>
2023-06-25 15:20:43 +0530

    Add files via upload

--- Config.java
+++ Config.java
@@ -1,13 +1,9 @@
-package ScreenShot;
+package absolute_Relative_xpath;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.io.FileHandler;
 
 public class Config {
 	WebDriver driver;
@@ -16,22 +12,33 @@ public class Config {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe.exe");
 		driver=new ChromeDriver();
 		driver.get(url);
-		
-		}
-	
-	public void takeScreenShot() {
-		File source=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		File target=new File("C:\\Users\\pwaware\\Desktop\\SeleniumWorkSpace\\ScreenShot\\PopupScreenshot.png");
-		
-		try {
-			FileHandler.copy(source, target);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
 	}
+	
+	public void accessElement() {
+		
+		//-----Absolute xpath html-body-div-a
+		//driver.findElement(By.xpath("/html/body/div/a[3]")).click();
+		
+		
+		//Relative xpath-->check unic attribute
+		driver.findElement(By.xpath("//a[@id='navbtn_exercises']")).click();
+		
+	}
+	
+	public void elementAccess() {
+		WebElement elementName=driver.findElement(By.xpath("//input[contains(@name,'firstName')]"));
+		elementName.sendKeys("Prashanti Waware");
+		
+		
+		//contains--> if 'password123' in this 'password' is constant(not changable) then use contains
+		driver.findElement(By.xpath("//input[contains(@name,'password')]")).sendKeys("1234");
+		
+          //starts-with-->if 'confirm_passwd123' in this 'confirm_passwd' is constant(not changable) then use starts-with		
+		driver.findElement(By.xpath("input[starts-with(@name,'confirm_passwd')]")).sendKeys("6767623");
+
+	}
+	
+	
 	
 
 }
---
+++ TestXPATH.java
@@ -0,0 +1,17 @@
+package absolute_Relative_xpath;
+
+public class TestXPATH extends Config {
+
+	public static void main(String[] args) {
+		TestXPATH obj=new TestXPATH();
+		//obj.driverSetting("https://www.w3schools.com/");
+		//obj.driverSetting("https://login.yahoo.com/account/create?specId=yidregsimplified&done=https%3A%2F%2Fwww.yahoo.com");
+		
+		obj.accessElement();
+		obj.elementAccess();
+		
+		
+
+	}
+
+}
