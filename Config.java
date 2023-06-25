07534c3d3ec86ae195f4c3dae20a8089c07dc8be
PrashantiWaware <137686962+PrashantiWaware@users.noreply.github.com>
2023-06-25 15:17:32 +0530

    Add files via upload

--- Config.java
+++ Config.java
@@ -1,105 +1,73 @@
-package DemoTable;
+package DemoonExcelSheet;
 
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Config {
-	WebDriver driver;
 	
-	public void driverSetting(String url) {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe.exe");
-		driver=new ChromeDriver();
-		driver.get(url);
-	}
 	
-	public  void loginAccess() {
-		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
-		driver.findElement(By.id("password")).sendKeys("123456");
-		driver.findElement(By.tagName("button")).click();
-		
-	}
-	
-	public void dashBoard() {
-		//---------------Click on users-------------------
-		driver.findElement(By.xpath("/html/body/div[1]/aside/section/ul/li[3]/a")).click();
-		
-		//Heading row
-		List <WebElement> list_row= driver.findElements(By.tagName("tr"));
-		System.out.println("==> Row Size==>"+list_row.size());
-		
-		for(int i=1;i<list_row.size();i++) {
-			//List <WebElement> list_col=driver.findElements(By.tagName("td"));
-			List <WebElement> list_col=list_row.get(i).findElements(By.tagName("td"));
-			//System.out.println("==>Col Size==>"+list_col.size());
-			for(int j=0;j<8;j++) {
-				System.out.print(list_col.get(j).getText()+"\t");
-			}
-			System.out.println("");
+	public void readExcelsheet() {
+		try {
+			FileInputStream fis=new FileInputStream("C:\\SeleniumWorkSpace\\ExcelSheets\\Mydata.xlsx");
 			
+			XSSFWorkbook workbook=new  XSSFWorkbook(fis);
+			XSSFSheet sheet=workbook.getSheet("Sheet1");
+			XSSFRow row=sheet.getRow(1);
+			XSSFCell cell=row.getCell(1);
 			
-			}
-		
-		
-	}
-	
-	public void operatorsDashboard() {
-		driver.findElement(By.xpath("/html/body/div[1]/aside/section/ul/li[4]/a/span")).click();
-		
-	     List <WebElement> list_row= driver.findElements(By.tagName("tr"));
-	     System.out.println("==>Row size==> "+list_row.size());
-	     
-	     for(int i=1;i<list_row.size();i++) {
-	    	//List<WebElement> list_col= driver.findElements(By.tagName("td"));
-	    	List <WebElement> list_col=list_row.get(i).findElements(By.tagName("td"));
-	    	//System.out.println("==>Col Size==> "+list_col.size());
-	    	for(int j=0;j<6;j++) {
-	    		System.out.print(list_col.get(j).getText()+"\t");
-	    	}
-	    	System.out.println();
-	     }
-	}
-	
-	public void usefulLinks() {
-		driver.findElement(By.xpath("/html/body/div/aside[1]/section/ul/li[5]/a/span")).click();
-		List <WebElement> list_row=driver.findElements(By.tagName("tr"));
-		System.out.println("==>Row Size==> "+list_row.size());
-		
-		for(int i=1;i<list_row.size();i++) {
-			//List <WebElement> list_col=driver.findElements(By.tagName("td"));
-			List<WebElement> list_col=list_row.get(i).findElements(By.tagName("td"));
+			System.out.println(cell.getStringCellValue());
+					
+		} catch (FileNotFoundException e) {
 			
-			//System.out.println("==Col Size== "+list_col.size());
-			for(int j=0;j<3;j++) {
-				System.out.print(list_col.get(j).getText()+"\t");
-			}
-			System.out.println();
+			e.printStackTrace();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
 		}
 	}
 	
-	public void downLoads() {
-		driver.findElement(By.xpath("/html/body/div/aside[1]/section/ul/li[6]/a/span")).click();
-		
-		List <WebElement> list_row=driver.findElements(By.tagName("tr"));
-		System.out.println("==Row size "+list_row.size());
-		
-		for(int i=1;i<list_row.size();i++) {
-			//List <WebElement> list_col=driver.findElements(By.tagName("td"));
-			List<WebElement> list_col=list_row.get(i).findElements(By.tagName("td"));
-			for(int j=0;j<8;j++) {
-				System.out.print(list_col.get(j).getText()+"\t");
-			}
-			System.out.println();
+	public void readAllData() {
+		try {
+			FileInputStream fis=new FileInputStream("C:\\\\SeleniumWorkSpace\\\\ExcelSheets\\\\Mydata.xlsx");
 			
+			XSSFWorkbook workbook=new XSSFWorkbook(fis);
+			XSSFSheet sheet=workbook.getSheet("Sheet1");
+			int rowCount=sheet.getLastRowNum();
+			
+			for(int i=0;i<=rowCount;i++) {
+				XSSFRow row=sheet.getRow(i);
+				
+				int cellCount=row.getLastCellNum();
+				
+				for(int j=0;j<cellCount;j++) {
+					XSSFCell cell=row.getCell(j);
+					
+					if(j==0 && i==1) {
+						System.out.print(cell.getNumericCellValue()+"\t");
+					}
+					else {
+						System.out.print(cell.getStringCellValue()+"\t");
+					}
+					
+				
+			}
+				System.out.println();
+		
+		} 
+		
+	}
+		
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
-	}
-	
-	public void logOut() {
-		driver.findElement(By.xpath("/html/body/div/aside/section/ul/li[7]/a/span")).click();
-	}
-	
-
+}
 }
